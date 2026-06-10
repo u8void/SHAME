858321632bf708c5028d37d8a68ee5311d5229a4
ahmedbarakat207 <ahmedbarakat2017-2007@outlook.com>
2026-06-11 00:20:35 +0300

    fuck

--- test_ddg.py
+++
@@ -1,8 +0,0 @@
-import re
-from duckduckgo_search import DDGS
-user_text = "Okay, can you think about Google's new Android 17?"
-filler = {"okay", "can", "you", "tell", "me", "about", "the", "new", "what", "is", "who", "where", "when", "how", "please", "search", "for", "look", "up", "a", "an", "of", "to", "in", "on", "and", "that", "this", "it", "some", "more", "details", "info", "information", "hey", "iris", "do", "know", "anything", "google's", "googles"}
-clean = re.sub(r'[^\w\s]', '', user_text.lower())
-kw = " ".join([w for w in clean.split() if w not in filler])
-print(f"Fallback KW: {kw}")
-print(DDGS().text(kw, max_results=2))
