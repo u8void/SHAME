a11bad969471446a448ba620ac9268978e876245
ahmedbarakat207 <ahmedbarakat2017-2007@outlook.com>
2026-07-01 22:11:51 +0300

    My Mosculus

--- test.py
+++
@@ -1,7 +0,0 @@
-import re
-s = '$$ \n ```HTML\n <span style="font-weight: bold;">1</span>\n'
-print("BEFORE:")
-print(s)
-s = re.sub(r'```[\s\S]*?(?:```|$)', '', s, flags=re.IGNORECASE)
-print("AFTER:")
-print(s)
