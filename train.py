cc07e51709947ac0e9690282bf2a82f7cbd24390
hamdyahmed17 <hamdyahmed1750@gmail.com>
2026-07-04 07:09:57 +0300

    logs fix

--- train.py
+++ train.py
@@ -1272,12 +1272,24 @@ def download_all_models(roles_to_train: List[str] = None):
                 try:
                     os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
                     from huggingface_hub import hf_hub_download
+                    from tqdm import tqdm
+                    class ForceTqdm(tqdm):
+                        def __init__(self, *args, **kwargs):
+                            kwargs['disable'] = False
+                            super().__init__(*args, **kwargs)
+
                     parts = url.split("huggingface.co/")[-1].split("/resolve/")
                     repo_id = parts[0]
                     subparts = parts[1].split("/")
                     remote_name = "/".join(subparts[1:])
                     print("  Using accelerated hf_transfer...")
-                    dl_path = hf_hub_download(repo_id=repo_id, filename=remote_name, local_dir="./models", local_dir_use_symlinks=False)
+                    dl_path = hf_hub_download(
+                        repo_id=repo_id,
+                        filename=remote_name,
+                        local_dir="./models",
+                        local_dir_use_symlinks=False,
+                        tqdm_class=ForceTqdm
+                    )
                     if os.path.abspath(dl_path) != os.path.abspath(dest_path):
                         if os.path.exists(dest_path): os.remove(dest_path)
                         os.rename(dl_path, dest_path)
