447368df6f12457abf6cf6c647e96a20365b0863
hamdyahmed17 <hamdyahmed1750@gmail.com>
2026-07-15 10:14:10 +0300

    kota may cry

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 os.environ["GGML_CUDA_NO_VMM"] = "1"
 import sys
 os.environ["HF_XET_HIGH_PERFORMANCE"] = "1"
-os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "0"
 import json
 import random
 import argparse
@@ -1312,7 +1311,6 @@ def download_all_models(roles_to_train: List[str] = None):
     import urllib.request
     import time
 
-    os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "0"
     os.makedirs("./models", exist_ok=True)
 
     
